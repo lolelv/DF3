e4f682f18e756e4a08014ac26d30ae190414c38a
lolelv <jfcastellanosq@unal.edu.co>
2016-02-27 07:42:37 -0500

    new commit

---
+++ src/df3/DF3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package df3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class DF3 {
+    static void Func1(){
+        System.out.println("Segunda Funcion");
+    }
+    static void Func2(){
+        System.out.println("Tercera Funcion");
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primera Función en Main"); 
+        Func1();
+        Func2();
+        System.out.println("Se termina Main");
+        }
+    }
+
+    
+    
+
